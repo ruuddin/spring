660a757a4b7766c5805acd5f5af07d0c6a2faec7
Riaz Uddin <ruddin@nisum.com>
2017-08-01 18:25:29 -0700

    spring-scheduler to schedule job at fixed rate

--- spring-scheduler/src/main/java/hello/ScheduledTasks.java
+++ spring-scheduler/src/main/java/hello/ScheduledTasks.java
@@ -1,17 +1,22 @@
 package hello;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-@RestController
-public class HelloController {
-	@Value(value="${greeting.msg}")
-	private String greeting;
-	
-	@GetMapping
-	public String hello(){
-		return greeting;
-	}
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
 }
