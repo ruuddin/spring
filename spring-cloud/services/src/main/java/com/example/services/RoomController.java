9a17cf449afd04f170623cd0c794acc2d08a7a2d
Riaz Uddin <riaz.uddin@teradata.com>
2018-08-08 17:16:09 -0700

    spring Eureka client registration and discovery example

--- spring-cloud/services/src/main/java/com/example/services/RoomController.java
+++ spring-cloud/services/src/main/java/com/example/services/RoomController.java
@@ -2,7 +2,10 @@ package com.example.services;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +24,9 @@ public class RoomController {
     private RoomServices roomServices;
 
     @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
     public RoomController(RoomServices roomServices) {
         super();
         this.roomServices = roomServices;
@@ -28,10 +34,17 @@ public class RoomController {
 
     @GetMapping
     @ApiOperation(value = "Get all rooms in the system", nickname = "getAllRooms")
-    public List<Room> getRooms(@RequestParam(name="roomNumber", required=false)String roomNumber) {
-        if(StringUtils.isNotBlank(roomNumber))
+    public List<Room> getRooms(@RequestParam(name = "roomNumber", required = false) String roomNumber) {
+        if (StringUtils.isNotBlank(roomNumber)) {
             return Collections.singletonList(this.roomServices.findByRoomNumber(roomNumber));
+        }
 
         return this.roomServices.getAllRooms();
+    }
+
+    @GetMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+        @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
     }
 }
